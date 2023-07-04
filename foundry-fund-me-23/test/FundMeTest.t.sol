27e0723a164f22a25217c5e875c3ca2978b6ab84
Ed Marcavage <edmarcavage256@proton.me>
2023-07-04 14:00:31 -0400

    Adds Modula refactor

--- foundry-fund-me-23/test/FundMeTest.t.sol
+++ foundry-fund-me-23/test/FundMeTest.t.sol
@@ -10,11 +10,15 @@ contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() external {
-        fundMe = new FundMe();
+        fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
     }
 
     function testMinimumDollarIsFive() public {
         uint256 minimumDollar = fundMe.MINIMUM_USD();
         assertEq(minimumDollar, 5e18);
+    }
+
+    function testOwnerIsMsgSender() public {
+        assertEq(fundMe.i_owner(), address(this));
     }
 }
